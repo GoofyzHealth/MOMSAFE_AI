9cec29e5a0a357265186161890d8ff4c72a1a2b0
Goofyz <129243293+GoofyzHealth@users.noreply.github.com>
2024-05-30 21:33:43 +0700

    Update pe_web_app.py

--- pe_web_app.py
+++ pe_web_app.py
@@ -379,10 +379,10 @@ if tabs == 'Deteksi Dini':
         puskesmas = st.selectbox('Wilayah Puskesmas', daftar_puskesmas)
     
     with col1:
-        nomor_nik = st.number_input('Nomor Induk Keluarga', format="%d", step=1, min_value=0, max_value=9999999999999999)
+        nomor_nik = st.number_input('Nomor Induk Keluarga', min_value=1111111111111111, max_value=9999999999999999, step=1.0, format="%.0f")
         
     with col2:
-        nomor_telp = st.number_input('Nomor Telepon', format="%d", step=1, min_value=0, max_value=999999999999)
+        nomor_telp = st.number_input('Nomor Telepon', min_value=10000000000, max_value=99999999999, step=1, format="%d")
     
     #Kolom Data Ibu
     st.subheader('Data Pemeriksaan Ibu')
